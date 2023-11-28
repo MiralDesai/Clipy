96d81c0f273f8adba19fc460bdd1f2143d696bc0
Miral Desai <13769268+MiralDesai@users.noreply.github.com>
2023-11-28 21:25:11 +0000

    feat: add realm file to handle migrations in the future

---
+++ Clipy/Config/Realm.swift
@@ -0,0 +1,21 @@
+//
+//  Realm.swift
+//  Clipy
+//
+//  Created by Miral Desai on 27/11/2023.
+//
+
+import RealmSwift
+
+extension Realm {
+  static func migration() {
+    let config = Realm.Configuration(schemaVersion: 0, migrationBlock: { migration, oldSchemaVersion in
+    })
+
+    Realm.Configuration.defaultConfiguration = config
+
+    // swiftlint:disable force_try
+    _ = try! Realm()
+    // swiftlint:enable force_try
+  }
+}
