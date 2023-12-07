7dae16ab75bc7d8dcda468754f1e1b91838314ec
Miral Desai <13769268+MiralDesai@users.noreply.github.com>
2023-12-07 21:05:39 +0000

    feat: menu bar item to delete all realm data

--- Clipy/Views/Menubar/AppMenu.swift
+++ Clipy/Views/Menubar/AppMenu.swift
@@ -6,12 +6,21 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 struct AppMenu: View {
   var body: some View {
     Divider()
+    Button("Clear history") { clearClipboardHistory() }.keyboardShortcut(KeyEquivalent.delete, modifiers: [.command, .option])
     SettingsLink {Text("Preferences")}.keyboardShortcut(",")
     Button("Quit") {NSApplication.shared.terminate(nil)}.keyboardShortcut("Q")
+  }
+}
+
+func clearClipboardHistory() {
+  let realm = try! Realm()
+  try! realm.write {
+    realm.deleteAll()
   }
 }
 
