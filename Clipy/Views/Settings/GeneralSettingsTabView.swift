eb03e130f332a588dfd12526d516200f4fa3c64d
Miral Desai <13769268+MiralDesai@users.noreply.github.com>
2024-01-19 00:19:24 +0000

    feat: add user options for paste immediately and paste with formatting
    design of settings still needs to be addressed but functionality is
    there. Also added an indicator for the accessability permission that is
    required to paste, read only for now

--- Clipy/Views/Settings/GeneralSettingsTabView.swift
+++ Clipy/Views/Settings/GeneralSettingsTabView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 import KeyboardShortcuts
 
 struct GeneralSettingsTabView: View {
-    var body: some View {
-      Form {
-        KeyboardShortcuts.Recorder("Open menu:", name: .openMenu)
-      }
+  @AppStorage(Constants.AppStorageKeys.pasteImmediately) private var pasteImmediately: Bool = true
+  @AppStorage(Constants.AppStorageKeys.pasteWithFormatting) private var pasteWithFormatting: Bool = false
+  private var permissedAllowed: Bool { AXIsProcessTrustedWithOptions(nil) }
+
+  var body: some View {
+    NavigationStack {
+      VStack(alignment: .leading) {
+        Form {
+          KeyboardShortcuts.Recorder("Open menu:", name: .openMenu)
+        }
+        Divider()
+        Toggle(isOn: $pasteImmediately) {
+          Text("Paste immediately")
+          Text("some descriptions about this toggle")
+        }.toggleStyle(.switch).controlSize(.mini)
+        Toggle(isOn: $pasteWithFormatting) {
+          Text("Paste with formatting")
+        }.toggleStyle(.switch).controlSize(.mini)
+        Divider()
+        HStack {
+          Image(systemName: "accessibility.fill")
+            .foregroundColor(.red)
+            .symbolRenderingMode(.hierarchical)
+            .font(.system(size: 30))
+          Text("Your account")
+        }
+      }.padding()
     }
+  }
 }
 
 #Preview {
-    GeneralSettingsTabView()
+  GeneralSettingsTabView()
 }
